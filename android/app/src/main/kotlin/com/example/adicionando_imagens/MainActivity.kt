e5a53ec97b175c28c5eb83c7362e47540fe7fc13
Dalander <bigdatahi7@gmail.com>
2025-06-23 13:39:37 -0300

    Adicionando imagens ao app

---
+++ android/app/src/main/kotlin/com/example/adicionando_imagens/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.adicionando_imagens
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MeuApp());
+
+
+
+class MeuApp extends StatelessWidget {
+  @override
+  Widget build (BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Adicionando imagem")
+        ),
+        body: Column(
+          children: [  
+
+            //ADICIONANDO IMAGEM LOCALMENTE
+            //EDITA O DIRETÓRIO pubspec.yaml
+            //ADICIONA O DIRETORIO NOVO QUE VOCÊ CRIOU CHAMADO ASSETS
+            Image(image: 
+              AssetImage("assets/logo.jpg"),
+              width: double.infinity,
+              fit: BoxFit.fitWidth,
+           
+            ), 
+            //ADICIONANDO IMAGEM DA ITERNET
+            Image.network('https://www.google.com.br/google.jpg'),
+
+          ],
+        ),
+      ),
+      
+    );
+  }
+}
